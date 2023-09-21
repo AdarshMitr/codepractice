c550424c2ace0c158cd1c250e7ce01e1b9caa63f
AdarshMitr <adarshkumar019@gmail.com>
2023-09-21 19:46:35 +0530

    sum of numbers by prompt

--- 2.SumofNumbers.js
+++ 2.SumofNumbers.js
@@ -3,4 +3,14 @@ function SumofNum(a,b){
     return a+b;
 }
 
-console.log(SumofNum(55,32));
+//console.log(SumofNum(55,32));
+
+//document.getElementById('box').innerHTML=SumofNum(33,40);
+
+//Sum of Numbers entered by user:
+const num1=parseInt(prompt('enter first number'));
+const num2=parseInt(prompt('enter second number'));
+const sum=num1+num2;
+document.getElementById('box').innerHTML=sum;
+
+
